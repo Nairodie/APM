229152047e6db38032412dd8848c085dff5998b3
Nairodie <42880967+Nairodie@users.noreply.github.com>
2020-08-11 16:56:24 -0500

    Adding services and using dependency injection

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
     selector: "pm-products",
@@ -23,46 +24,9 @@ export class ProductListComponent implements OnInit{
     }
 
     filteredProducts: IProduct[];
-    products: IProduct[] = [
-            {
-              "productId": 1,
-              "productName": "Ulysses",
-              "Author": "James Joyce",
-              "productCode": "GDN-0011",
-              "releaseDate": "1922",
-              "description": "Ulysses chronicles the peripatetic appointments and encounters of Leopold Bloom in Dublin in the course of an ordinary day, 16 June 1904",
-              "price": 19.95,
-              "starRating": 5,
-              "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/a/ab/JoyceUlysses2.jpg"
-            },
-            {
-                "productId": 2,
-                "productName": "The Great Gatsby",
-                "Author": "F. Scott Fitzgerald",
-                "productCode": "GDN-0023",
-                "releaseDate": "1925",
-                "description": "Set in Jazz Age New York, the novel tells the tragic story of Jay Gatsby, a self-made millionaire, and his pursuit of Daisy Buchanan, a wealthy young woman whom he loved in his youth",
-                "price": 9.95,
-                "starRating": 4.5,
-                "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/a/a0/The_Great_Gatsby_cover_1925_%281%29.jpg"
-            },
-            {
-                "productId": 3,
-                "productName": "Winter of Fire",
-                "Author": "Sherryl Jordan",
-                "productCode": "GDN-0033",
-                "releaseDate": "1933",
-                "description": "todo",
-                "price": 4.95,
-                "starRating": 5,
-                "imageUrl": "https://images-na.ssl-images-amazon.com/images/I/81B2xmUcGbL.jpg"
-            }     
-              
-        ];
+    products: IProduct[] = [];
 
-        constructor() {
-            this.filteredProducts = this.products;
-            this.listFilter = "winter";
+        constructor (private productService: ProductService) {
         }
 
         onRatingClicked(message: string): void {
@@ -78,7 +42,9 @@ export class ProductListComponent implements OnInit{
         toggleImage(): void {
             this.showImage = !this.showImage;
         }
+
         ngOnInit(): void {
-            console.log("In OnItit");
+            this.products = this.productService.getProducts();
+            this.filteredProducts = this.products;
         }
 }
---
+++ src/app/products/product.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from "@angular/core";
+import { IProduct } from "./product";
+
+@Injectable({
+    providedIn: "root"
+})
+
+export class ProductService {
+    getProducts(): IProduct[] {
+        return [
+            {
+                "productId": 1,
+                "productName": "Ulysses",
+                "Author": "James Joyce",
+                "productCode": "GDN-0011",
+                "releaseDate": "1922",
+                "description": "Ulysses chronicles the peripatetic appointments and encounters of Leopold Bloom in Dublin in the course of an ordinary day, 16 June 1904",
+                "price": 19.95,
+                "starRating": 5,
+                "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/a/ab/JoyceUlysses2.jpg"
+              },
+              {
+                  "productId": 2,
+                  "productName": "The Great Gatsby",
+                  "Author": "F. Scott Fitzgerald",
+                  "productCode": "GDN-0023",
+                  "releaseDate": "1925",
+                  "description": "Set in Jazz Age New York, the novel tells the tragic story of Jay Gatsby, a self-made millionaire, and his pursuit of Daisy Buchanan, a wealthy young woman whom he loved in his youth",
+                  "price": 9.95,
+                  "starRating": 4.5,
+                  "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/a/a0/The_Great_Gatsby_cover_1925_%281%29.jpg"
+              },
+              {
+                  "productId": 3,
+                  "productName": "Winter of Fire",
+                  "Author": "Sherryl Jordan",
+                  "productCode": "GDN-0033",
+                  "releaseDate": "1933",
+                  "description": "todo",
+                  "price": 4.95,
+                  "starRating": 5,
+                  "imageUrl": "https://images-na.ssl-images-amazon.com/images/I/81B2xmUcGbL.jpg"
+              }     
+        ];
+    }
+}
